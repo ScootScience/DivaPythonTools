4b4ff0c8a2fc41b062d0c17813c351a19dbc1d89
Todd Bryan <todd.r.bryan@gmail.com>
2019-03-22 10:30:34 -0700

    Bump to 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE.md') as f:
 
 setup(
         name='DivaPythonTools',
-        version='0.1.0',
+        version='0.1.1',
         description='A set of Python tools to help users with: 1. The preparation of Diva input files: data, contours, parameters; 2. The execution of the Diva interpolation tool; 3. The reading of output files (analysis, finite-element mesh); 4. The input and output plotting.',
         long_description=readme,
         author='Charles Troupin',
